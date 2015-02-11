bbdf692c3c39f44605f5dcd9dc4fd0994929aa30
Chris Reid <chrisreiduk@gmail.com>
2015-02-11 11:33:31 +0000

    Setup resources.

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,53 @@ var elixir = require('laravel-elixir');
  |--------------------------------------------------------------------------
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as copying vendor resources.
  |
- */
+ bower_components/*/
 
-elixir(function(mix) {
-    mix.less('app.less');
+elixir(function (mix) {
+    mix
+        .copy(
+            'bower_components/bootstrap-sass-official/assets/stylesheets',
+            'resources/assets/sass/vendor'
+        )
+        .copy(
+            'bower_components/font-awesome/scss',
+            'resources/assets/sass/vendor/font-awesome'
+        )
+        .sass('app.scss', 'resources/assets/css')
+        .copy(
+            'bower_components/jquery/dist/jquery.js',
+            'resources/assets/js/vendor/jquery.js'
+        )
+        .copy(
+            'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+            'resources/assets/js/vendor/bootstrap.js'
+        )
+        .copy(
+            'bower_components/bootstrap-sass-official/assets/fonts/bootstrap',
+            'public/fonts'
+        )
+        .copy(
+            'bower_components/font-awesome/fonts',
+            'public/fonts'
+        )
+        .copy(
+            'bower_components/angular/angular.js',
+            'resources/assets/js/vendor/angular.js'
+        )
+        .styles([
+            'app.css',
+        ], 'public/css/all.css', 'resources/assets/css')
+        .scripts([
+            'vendor/jquery.js',
+            'vendor/bootstrap.js',
+            'vendor/angular.js',
+            'app.js'
+        ], 'public/js/all.js', 'resources/assets/js')
+        .version([
+            'public/css/all.css',
+            'public/js/all.js'
+        ]);
 });
